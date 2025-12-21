bce11258b3044f9dbc6a8b96146b250b8e9c6808
rafi <mail@rafi>
2025-12-21 21:03:49 +0100

    update server: new instance

--- start.bat
+++ start.bat
@@ -1,10 +1,16 @@
 @ECHO OFF
 
+cls
+
+if not exist fabric-server.jar (
+	curl -o fabric-server.jar -J https://meta.fabricmc.net/v2/versions/loader/1.21.11/0.18.3/1.1.0/server/jar
+)
+
 echo Server wird gestartet...
 echo Siehe unter anderem client side mods im client-mods-Verzeichnis
 
 git pull
 
-java -Xmx2G -jar fabric-server-mc.1.21.11-loader.0.18.3-launcher.1.1.0.jar nogui
+java -Xmx2G -jar fabric-server.jar nogui
 
 pause
